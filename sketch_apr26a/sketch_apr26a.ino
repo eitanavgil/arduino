c823bcbb3b5333323444a5d6147ccd477b9f29c4
Katya <Katya@Katyas-MacBook-Pro.local>
2018-04-26 22:54:46 +0300

    basic functional of buttons down states

--- sketch_apr26a/sketch_apr26a.ino
+++ sketch_apr26a/sketch_apr26a.ino
@@ -1,22 +1,27 @@
 #include <ButtonDebounce.h>
 int a = 0;
-ButtonDebounce button(10, 10);
-void buttonChanged(int state){
-  if(state == 0 ) {
-    Serial.println("DOWN: " + String(a) );
-    a++;
-  }else{
-    Serial.println("UP: ");
-  }
-  
-}
 
+ButtonDebounce button1(2, 10);
+ButtonDebounce button2(3, 10);
+
+void button1Changed(int state){
+  if(state == 0 ) {
+    Serial.println(String(a++) );
+  }
+}
+void button2Changed(int state){
+  if(state == 0 ) {
+    Serial.println(String(a--) );
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  button.setCallback(buttonChanged);
+  button1.setCallback(button1Changed);
+  button2.setCallback(button2Changed);
 }
 
 void loop() {
-  button.update();
+  button1.update();
+  button2.update();
 }
